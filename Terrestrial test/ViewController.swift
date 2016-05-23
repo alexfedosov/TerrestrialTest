dcc59872f0e7dfa905277e90d3b3c614a5592b67
Alexander Fedosov <alexander.a.fedosov@gmail.com>
2016-05-23 10:00:59 +0300

    Initial Commit

---
+++ Terrestrial test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Terrestrial test
+//
+//  Created by Alexander Fedosov on 23.05.16.
+//  Copyright © 2016 Alexander Fedosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
